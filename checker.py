158ce11bb376d1bd5cc903ae06e49f39ce330ba0
Mathew Oakes <support@mathewoak.es>
2017-02-03 19:01:13 +1100

    Setup for tests using libreoffice test_doc.odt

---
+++ checker.py
@@ -0,0 +1,23 @@
+import uno
+
+def get_desktop():
+    localContext = uno.getComponentContext()
+    resolver = localContext.ServiceManager.createInstanceWithContext(
+	"com.sun.star.bridge.UnoUrlResolver", localContext )
+    ctx = resolver.resolve(
+        "uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext")
+    smgr = ctx.ServiceManager
+    desktop = smgr.createInstanceWithContext(
+        "com.sun.star.frame.Desktop", ctx
+    )
+    return desktop
+
+def get_active_model(desktop):
+    desktop.setActiveFrame(desktop.getFrames()[0])
+    return desktop.getCurrentComponent()
+"""
+>>> from checker import *
+>>> desktop = get_desktop()
+>>> model = get_active_model(desktop)
+
+"""
--- test_GrammarAlert.py
+++ test_GrammarAlert.py
@@ -1,11 +1,14 @@
 import unittest
 from xml.etree import ElementTree as ET
-
+from checker import get_desktop, get_active_model
+import subprocess
+import os
+import time
 from GrammarAlert import run_check, decode_errors, \
-    check_sentence
+    check_sentence, segment
 
-class GTest(unittest.TestCase):
-    
+class ATD_XML_Test(unittest.TestCase):
+
     def test_run_check(self):
         sentence = '“This is a sentence”'
         response = run_check(sentence)
@@ -19,14 +22,13 @@ class GTest(unittest.TestCase):
         self.assertEqual(
             len(erroot), 0
         )
-        cerrors = check_sentence(sentence) 
+        cerrors = check_sentence(sentence)
         cerroot = cerrors.getroot()
         self.assertEqual(
             ET.tostring(erroot), ET.tostring(cerroot)
         )
-        
 
-    def test_run_check_longer(self):
+    def test_run_check_segment(self):
         sentence = 'It was determined by the committee that the report was inconclusive'
         response = run_check(sentence)
         self.assertEqual(
@@ -61,12 +63,55 @@ class GTest(unittest.TestCase):
 	                    'url'
                         )
                     )
-        cerrors = check_sentence(sentence) 
-        cerroot = cerrors.getroot()
+        segments = segment(sentence, erroot)
         self.assertEqual(
-            ET.tostring(erroot), ET.tostring(cerroot)
+            segments[0],
+            (None, 'It ', None)
         )
-        
+        self.assertEqual(
+            segments[1],
+            ('grammar', 'was determined by the', 'http://127.0.0.1:1049/info.slp?text=was+determined+by+the&tags=VBD%2FVBN%2FIN%2FDT&engine=3')
+        )
+        self.assertEqual(
+            segments[2],
+            (None,
+             ' committee that the report was inconclusive',
+             None
+            )
+        )
+
+class LO_Cursor_Test(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        if 'SOFFICE_BINARY' not in os.environ.keys():
+            raise RuntimeError('Please set environment variable SOFFICE_BINARY to the path to your soffice executable to run these tests.')
+        self.soffice = subprocess.Popen(
+            '{} --accept="socket,host=localhost,port=2002;urp;" --norestore --nologo --nodefault --headless "test_doc.odt"'.format(os.environ['SOFFICE_BINARY']),
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        self.desktop = None
+        start = time.time()
+        while self.desktop is None and time.time() - start < 30:
+            print(time.time() - start)
+            try:
+                self.desktop = get_desktop()
+            except Exception as e:
+                print(e)
+                time.sleep(5)
+        if self.desktop is None:
+            raise RuntimeError('Unable to connect to soffice')
+        self.model = get_active_model(self.desktop)
+
+    def test_segments_match_cursor(self):
+        print('sm', self.model)
+
+    @classmethod
+    def tearDownClass(self):
+        if not self.soffice.poll():
+            self.soffice.terminate()
 
 if __name__=='__main__':
     unittest.main()
